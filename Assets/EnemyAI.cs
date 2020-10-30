bc127b8a1fb5668144326f3f2bcf48ad9026013c
Lumbr <jumiciobi@gmail.com>
2020-10-30 19:45:21 +0000

    P A T H F I N D I N G  Y    E    S

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -48,7 +48,7 @@ public class EnemyAI : MonoBehaviour
         Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
         Vector2 force = direction * speed * Time.deltaTime;
         rb.velocity = force;
-        rb.AddForce(force/4);
+        rb.AddForce(force);
         float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
         if(distance < nextWaypointDistance)
         {
