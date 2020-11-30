a62b890e6fa8497607202e1b2bd9640e2dc1456a
Lumbr <jumiciobi@gmail.com>
2020-11-30 15:14:33 +0000

    added item drops

--- Assets/Scripts/Players-Entities/EnemyHealthManager.cs
+++ Assets/Scripts/Players-Entities/EnemyHealthManager.cs
@@ -1,13 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class EnemyHealthManager : MonoBehaviour
 {
     public int enemyMaxHealth;
     public int enemyCurrentHealth;
     public int expToGive;
-
+    [Serializable]
+    public struct ItemWithChance
+    {
+        public Item item;
+        public float chance;
+    }
+    public GameObject pfItemWorld;
+    public List<ItemWithChance> Drops;
+    //public Dictionary<Item,float> ActualDrops;
     public string enemyQuestName;
     private QuestManager theQM;
     // Start is called before the first frame update
@@ -15,6 +23,7 @@ public class EnemyHealthManager : MonoBehaviour
     {
         theQM = FindObjectOfType<QuestManager>();
         enemyCurrentHealth = enemyMaxHealth;
+        
     }
 
     // Update is called once per frame
@@ -24,8 +33,18 @@ public class EnemyHealthManager : MonoBehaviour
         {
             theQM.enemyKilled = enemyQuestName;
 
+            
+            float rand = UnityEngine.Random.Range(0, 100);
+            foreach (ItemWithChance thisItem in Drops)
+            {
+                rand = UnityEngine.Random.Range(0, 100);
+                if (rand >= thisItem.chance)
+                {
+                    ItemWorld.SpawnItemWorld(transform.position, thisItem.item);
+                    
+                }
+            }
             Destroy(gameObject);
-
         }
     }
     public void HurtEnemy(int damage) { enemyCurrentHealth -= damage; }
